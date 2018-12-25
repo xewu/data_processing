1d320d8e222d899a10bc87c98ebf43c54bf4e1aa
EricaWu <xue.wu.erica@gmail.com>
2018-12-25 14:53:14 -0800

    delete

--- data-consumer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-from kafka import KafkaConsumer
-
-if __name__ == "__main__":
-	consumer = KafkaConsumer('data_analyzer', bootstrap_servers='192.168.99.100:9092')
-	for msg in consumer:
-		print(msg)
